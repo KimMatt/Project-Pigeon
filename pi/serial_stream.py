f31dcb97610fce97f16277e44b44e28d036fe68f
anookday <anookday@gmail.com>
2020-12-17 21:01:53 +0900

    configured gui to display message from serial buffer

--- pi/serial_stream.py
+++ pi/serial_stream.py
@@ -1,24 +1,25 @@
 import time
 import serial
-from tkinter import *
+from tkinter import Tk, StringVar, Label
 
 window = Tk()
-window.title("Project Pigeon")
-window.attributes("-fullscreen", True)
+window.title('Project Pigeon')
+window.attributes('-fullscreen', True)
 
-message = Label(window, text="elonmusk: EZ Clap", font="Helvetica 48")
-message.place(relx=.5, rely=.5, anchor="center")
+message = StringVar()
+label = Label(window, textvariable=message, font='Helvetica 64')
+label.place(relx=.5, rely=.5, anchor='center')
 
 window.mainloop()
 
 try:
     ser = serial.Serial('/dev/ttyGS0', 115200, 8, 'N', 1, timeout=1)
-    output = ""
-    print("Receiving serial responses from connected USB host...")
+    output = ''
+    print('Receiving serial responses from connected USB host...')
     while True:
-        output = ser.readline()
+        output = ser.readline().decode('utf-8')
         if len(output) > 0:
-            print(output)
-        time.sleep(1)
-except (FileNotFoundError, serial.serialutil.SerialException):
-    print("Serial port not found.")
+            message.set(output)
+        time.sleep(5)
+except (FileNotFoundError, serial.SerialException):
+    print('Serial port not found.')
