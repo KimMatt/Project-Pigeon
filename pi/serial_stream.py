e84f0c191c5b42db008e7aea43d10af39e0df81c
anookday <anookday@gmail.com>
2020-12-17 16:35:34 +0900

    added python gui

--- pi/serial_stream.py
+++ pi/serial_stream.py
@@ -1,10 +1,24 @@
+import time
 import serial
+from tkinter import *
 
-ser = serial.Serial('/dev/ttyGS0', 115200, 8, 'N', 1, timeout=1)
-output = ""
+window = Tk()
+window.title("Project Pigeon")
+window.attributes("-fullscreen", True)
 
-print("Receiving serial responses from connected USB host...")
-while True:
-    output = ser.readline()
-    if len(output) > 0:
-        print(output)
+message = Label(window, text="elonmusk: EZ Clap", font="Helvetica 48")
+message.place(relx=.5, rely=.5, anchor="center")
+
+window.mainloop()
+
+try:
+    ser = serial.Serial('/dev/ttyGS0', 115200, 8, 'N', 1, timeout=1)
+    output = ""
+    print("Receiving serial responses from connected USB host...")
+    while True:
+        output = ser.readline()
+        if len(output) > 0:
+            print(output)
+        time.sleep(1)
+except (FileNotFoundError, serial.serialutil.SerialException):
+    print("Serial port not found.")
