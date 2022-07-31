93e9ad43c48f8176db8cc6fd1a67d37b09645c31
freddyshim <shimfreddy@gmail.com>
2022-07-31 15:29:48 -0400

    update pi script with auto exposure toggle commands

--- pi/serial_stream.py
+++ pi/serial_stream.py
@@ -1,8 +1,31 @@
 import time
 import serial
+import os
 from tkinter import Tk, StringVar, Label
 
-def readSerial():
+COMM_LENGTH = 4
+
+def updateGUI(window: Tk, textInput: StringVar, message: str):
+	textInput.set(message)
+	window.update()
+
+def toggleAutoExposure(toggle: str):
+	controls = { '0', '1' }
+	if toggle in controls:
+		os.system(f'/usr/bin/v4l2-ctl -c auto_exposure={toggle}')
+	else:
+		print(f'Error: Invalid command. Expected {", ".join(controls)} but got {toggle}.')
+
+def parseCommand(window: Tk, textInput: StringVar, command: str):
+	if len(command) < COMM_LENGTH:
+		return
+	comm = command[:COMM_LENGTH -1]
+	return {
+		'CHAT': updateGUI(window, textInput, command[COMM_LENGTH + 1:]),
+		'EXPO': toggleAutoExposure(command[COMM_LENGTH + 1:])
+	}[comm]
+
+def readSerial(window: Tk, textInput: StringVar):
     while True:            
         try:
             ser = serial.Serial('/dev/ttyGS0', 115200, 8, 'N', 1, timeout=1)
@@ -11,8 +34,8 @@ def readSerial():
             while True:
                 output = ser.readline().decode('utf-8')
                 if len(output) > 0:
-                    message.set(output)
-                    window.update()
+                    parseCommand(window, textInput, output)
+
         except (FileNotFoundError, serial.SerialException):
             print('Serial port not found.')
             time.sleep(1)
@@ -24,8 +47,8 @@ window.attributes('-fullscreen', True)
 window.configure(background='black')
 window.config(cursor='none')
 
-message = StringVar()
-label = Label(window, textvariable=message, font='Helvetica 64', wraplength=700, fg='#fff')
+textInput = StringVar()
+label = Label(window, textvariable=textInput, font='Helvetica 64', wraplength=700, fg='#fff')
 label.place(relx=0, rely=.5, anchor='w')
 
 window.after(2000, readSerial)
